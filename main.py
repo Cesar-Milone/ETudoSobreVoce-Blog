a69354fa46816f2ea29a42f53344dc7754da2f14
Milone <86480000+Cesar-Milone@users.noreply.github.com>
2021-10-16 00:50:13 -0300

    Bible API in run

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template, jsonify
 import json
 from bible_api import Bible
+from datetime import datetime
 
 app = Flask(__name__)
 bible = Bible()
+today = datetime.now().day
+bible_num = 1
+bible_text = ""
+
+
+def change_bible_text():
+    global today, bible_num, bible_text
+    new_day = datetime.now().day
+    if today != new_day:
+        bible.get_versivulo()
+        today = new_day
+    if bible_num == 1:
+        bible_num = 2
+        bible_text = bible.text1
+    elif bible_num == 2:
+        bible_num = 1
+        bible_text = bible.text2
+
 
 with open("static/json_data.json", "r") as file:
     post_completed = json.load(file)
@@ -15,7 +34,8 @@ post_body = post_completed["0"]["post_body"]
 
 @app.route("/")
 def index():
-    return render_template("index.html", bible=bible.text1, post_title=post_title,
+    change_bible_text()
+    return render_template("index.html", bible=bible_text, post_title=post_title,
                            post_subtitle=post_subtitle, post_body=post_body, post_completed=post_completed)
 
 
