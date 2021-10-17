6849611b7da8a8c3184bc2e3b87a440d6fdc1752
Milone <86480000+Cesar-Milone@users.noreply.github.com>
2021-10-17 16:36:03 -0300

    Insert log info

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from datetime import datetime
 from static.classes.db import Database
 from random import randint
 import pandas as pd
+import logging
+
+logging.info("Startin application")
+print("Startin application")
 
 app = Flask(__name__)
 bible = Bible()
@@ -17,6 +21,7 @@ bible_text = ""
 
 def change_bible_text():
     global today, bible_num, bible_text
+    logging.info("bible webscrapping")
     new_day = datetime.now().day
     if today != new_day:
         bible.get_versivulo()
@@ -28,21 +33,24 @@ def change_bible_text():
         bible_num = 1
         bible_text = bible.text2
 
-
+logging.info("Open json")
 with open("static/json_data.json", "r") as file:
+    logging.info("Json opened")
     post_list = json.load(file)
+
 # Start values to test de DB in server
 for post in post_list:
+    logging.info("Starting DB configuration")
     db.post_title = post_list[post]["post_title"]
     db.post_subtitle = post_list[post]["post_subtitle"]
     db.post_body = post_list[post]["post_body"]
     db.user_id = 1
     db.comment_id = 1
     db.exec_insert()
-
+logging.info("Finish DB configuration")
 post_list_db = db.exec_select()
 post_pandas = db.exec_select_pandas()
-print(post_pandas.post_title[0])
+logging.info("First entering in index.html")
 
 
 @app.route("/")
